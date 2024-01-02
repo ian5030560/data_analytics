6bb528a4f1363485987443badb319d3d339c97d7
ian5030560 <tom5030560@gmail.com>
2024-01-02 18:48:01 +0800

    Update db.py

--- project/venv/myproject/db.py
+++ project/venv/myproject/db.py
@@ -1,8 +1,9 @@
 import sqlite3
 import time
 
+
 # 尋找該年級每個地區的答題數、答對題數、總人數
-def getData(grade: int)-> list[list[str, int, int, int]]:
+def getData(grade: int) -> list[list[str, int, int, int]]:
     """
     @return list[list[str, int, int, int]]: a list includes many lists of 地區、答對題數、總人數
     """
@@ -10,7 +11,7 @@ def getData(grade: int)-> list[list[str, int, int, int]]:
 
     cursor = con.cursor()
     g = f"grade{grade}"
-    
+
     cursor.execute(
         f"""SELECT user.city, COUNT({g}.upid) AS "total_problem", 
         SUM(CASE WHEN {g}.is_correct = 1 THEN 1 ELSE 0 END) AS "total_correct", 
@@ -20,14 +21,19 @@ def getData(grade: int)-> list[list[str, int, int, int]]:
         GROUP BY user.city;
         """
     )
+    cursor.fetchmany
     return list(map(list, cursor.fetchall()))
-    
-speedList = [None for _ in range(12)]
-def testSpeed(grade: int):
-    s = time.time()
-    getData(grade)
-    e = time.time()
-    speedList[grade - 1] = e - s
+
+
+# speedList = [None for _ in range(12)]
+
+
+# def testSpeed(grade: int):
+#     s = time.time()
+#     getData(grade)
+#     e = time.time()
+#     speedList[grade - 1] = e - s
+
 
 REGIONS = {
     "chc": "彰化",
@@ -49,16 +55,27 @@ REGIONS = {
     "tp": "台北",
     "ttct": "台東",
     "ty": "桃園",
-    "ylc": "雲林"
+    "ylc": "雲林",
 }
-        
+
+
+# if __name__ == "__main__":
+#     con = sqlite3.connect("sql.db")
+
+#     cursor = con.cursor()
     
+#     # km: 11 
+#     # lj: 9, 12
+#     g = 9
+#     city = "lj"
+#     cursor.execute(
+#         f"""SELECT COUNT(1)
+#         FROM problem
+#         JOIN "user" ON "user".uuid = problem.uuid
+#         WHERE "user".city = "{city}" AND "user".grade = {g} 
+#         """
+#     )
     
-if __name__ == "__main__":
-    print(getData(8))
-    # for t in range(len(speedList)):
-    #     testSpeed(t + 1)
-        
-    # print(f"平均值: {sum(speedList) / len(speedList)}")
-    # print(f"最大值: {max(speedList)}")
-    # print(f"最小值: {min(speedList)}")
+#     print(cursor.fetchall())
+#     cursor.close()
+#     con.close()
